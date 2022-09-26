03a825ec8e12dae3b8c3da13667ccea3f5609c21
itmakesnos3nse <imelchakov@gmail.com>
2022-09-26 19:05:35 +0300

    Initial commit

---
+++ yandex123/main/urls.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
